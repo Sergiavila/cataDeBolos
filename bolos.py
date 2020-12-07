6581632dbd995f963400959f7aed63b4f0d412a2
Sergio <sergiolopez382@gmail.com>
2020-12-07 17:48:33 +0100

    Test 7 OK

--- bolos.py
+++ bolos.py
@@ -1,22 +1,26 @@
 class Partida():
 	def calcularResultado(self,ronda):
 		suma = 0
-		for i in range(0, len(ronda)):
-			if sum(ronda[i]) <= 10:
-				if i == 9:
-					if(ronda[i][0] == 10):
-						suma += sum (ronda[i]) + sum(ronda[i+1])
-					elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
-						suma += sum(ronda[i]) + ronda[i+1][0]
-					else:
-						suma += sum(ronda[i])
-				elif i < 9:
-					if(ronda[i][0] == 10):
-						suma += sum (ronda[i]) + sum(ronda[i+1])
-					elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
-						suma += sum(ronda[i]) + ronda[i+1][0]
-					else:
-						suma += sum(ronda[i])
-				else:
-						suma += sum(ronda[i])
-		return suma
+		try:
+			if len(ronda) < 12:
+				for i in range(0, len(ronda)):
+					if sum(ronda[i]) <= 10:
+						if i == 9:
+							if(ronda[i][0] == 10):
+								suma += sum (ronda[i]) + sum(ronda[i+1])
+							elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
+								suma += sum(ronda[i]) + ronda[i+1][0]
+							else:
+								suma += sum(ronda[i])
+						elif i < 9:
+							if(ronda[i][0] == 10):
+								suma += sum (ronda[i]) + sum(ronda[i+1])
+							elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
+								suma += sum(ronda[i]) + ronda[i+1][0]
+							else:
+								suma += sum(ronda[i])
+						else:
+								suma += sum(ronda[i])
+			return suma
+		except:
+			print ("Solo se permiten introducir 11 rondas")

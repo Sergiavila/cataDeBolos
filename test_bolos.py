6d5e9fad27fc9ef1c61e1b73ef3e5e1c9c7ef495
Sergio <sergiolopez382@gmail.com>
2020-12-07 16:05:22 +0100

    Test 2 más complicado  OK

--- test_bolos.py
+++ test_bolos.py
@@ -14,3 +14,9 @@ class PartidaBolos(unittest.TestCase):
 		resultado = partida.calcularResultado(ronda)
 		self.assertEqual(resultado,1)
 
+	def test_partida_varios_puntos(self):
+		partida = Partida()
+		ronda = [(3,2),(0,1),(0,5),(6,1),(2,3),(4,5),(6,0),(0,0),(0,0),(0,0)]
+		resultado = partida.calcularResultado(ronda)
+		self.assertEqual(resultado,38)
+

e615913ee756866d943867031be33d552d72cfea
Sergio <sergiolopez382@gmail.com>
2020-12-07 17:37:44 +0100

    Test 6 OK

--- bolos.py
+++ bolos.py
@@ -2,20 +2,21 @@ class Partida():
 	def calcularResultado(self,ronda):
 		suma = 0
 		for i in range(0, len(ronda)):
-			if i == 9:
-				if(ronda[i][0] == 10):
-					suma += sum (ronda[i]) + sum(ronda[i+1])
-				elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
-					suma += sum(ronda[i]) + ronda[i+1][0]
+			if sum(ronda[i]) <= 10:
+				if i == 9:
+					if(ronda[i][0] == 10):
+						suma += sum (ronda[i]) + sum(ronda[i+1])
+					elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
+						suma += sum(ronda[i]) + ronda[i+1][0]
+					else:
+						suma += sum(ronda[i])
+				elif i < 9:
+					if(ronda[i][0] == 10):
+						suma += sum (ronda[i]) + sum(ronda[i+1])
+					elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
+						suma += sum(ronda[i]) + ronda[i+1][0]
+					else:
+						suma += sum(ronda[i])
 				else:
-					suma += sum(ronda[i])
-			elif i < 9:
-				if(ronda[i][0] == 10):
-					suma += sum (ronda[i]) + sum(ronda[i+1])
-				elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
-					suma += sum(ronda[i]) + ronda[i+1][0]
-				else:
-					suma += sum(ronda[i])
-			else:
-				suma += sum(ronda[i])
+						suma += sum(ronda[i])
 		return suma

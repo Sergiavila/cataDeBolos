3b2e5a8e613bdf9f0cd596ede4e2c132eec7af4d
Sergio <sergiolopez382@gmail.com>
2020-12-07 16:35:42 +0100

    Test 4 OK

--- bolos.py
+++ bolos.py
@@ -4,6 +4,8 @@ class Partida():
 		for i in range(0, len(ronda)):
 			if(ronda[i][0] == 10):
 				suma += sum (ronda[i]) + sum(ronda[i+1])
+			elif(sum(ronda[i]) == 10 and ronda[i][0] != 10):
+				suma += sum(ronda[i]) + ronda[i+1][0]
 			else:
 				suma += sum(ronda[i])
 		return suma
